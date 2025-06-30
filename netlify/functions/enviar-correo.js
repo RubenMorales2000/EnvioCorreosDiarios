6dcc0c38903d7c1c9f082eb5bfbfe9cdb69f9fce
RMorales <r.morales@simlevante.com>
2025-06-30 15:26:54 +0200

    Quitado workflow

--- netlify/functions/enviar-correo.js
+++ netlify/functions/enviar-correo.js
@@ -9,28 +9,15 @@ exports.handler = async function () {
     },
   });
 
-  try {
-    const info = await transporter.sendMail({
-      from: `"Yo Diario" <${process.env.EMAIL_USER}>`,
-      to: process.env.EMAIL_DEST,
-      subject: "Informe comida diario",
-      text: "https://forms.office.com/Pages/ResponsePage.aspx?id=FBHdc33vx0CGaVadMufim2AMPA_octtFlOXoE2JKGbJUMUlIUFpMTDc0WlZDME1LVFc1WEJQM0JNNC4u",
-    });
+  const info = await transporter.sendMail({
+    from: `"Yo Diario" <${process.env.EMAIL_USER}>`,
+    to: process.env.EMAIL_DEST,
+    subject: "Informe comida diario",
+    text: "https://forms.office.com/Pages/ResponsePage.aspx?id=FBHdc33vx0CGaVadMufim2AMPA_octtFlOXoE2JKGbJUMUlIUFpMTDc0WlZDME1LVFc1WEJQM0JNNC4u",
+  });
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Correo enviado", info }),
-    };
-  } catch (error) {
-    console.error("Error enviando el correo:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Error al enviar el correo", error: error.message }),
-    };
-  }
-};
-
-// 🕒 Programar ejecución cada 15 minutos
-exports.config = {
-  schedule: "*/15 * * * *",
+  return {
+    statusCode: 200,
+    body: JSON.stringify({message:"Correo enviado", info }),
+  };
 };
