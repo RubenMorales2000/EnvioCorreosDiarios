b5c50be86964d2c80bb8e230d378ae0353c44a8d
RMorales <r.morales@simlevante.com>
2025-07-01 10:29:28 +0200

    Recordatorio medicación

---
+++ netlify/functions/RecordatorioMed.js
@@ -0,0 +1,23 @@
+import { createTransport } from "nodemailer";
+
+export async function handler () {
+  const transporter = createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: `<${process.env.EMAIL_USER}>`,
+    to: process.env.EMAIL_DEST,
+    subject: "Recordatorio de tomar la medicación",
+    text: "Hola, este es un recordatorio para que tomes tu medicación diaria. Por favor, asegúrate de seguir las indicaciones de tu médico. Te quiero mucho mi PCR ❤️",
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({message:"Correo enviado", info}),
+  };
+}
